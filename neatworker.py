490a8224db94c2f06f46c1679ad9ad924fbaca18
EKKOING <nicholas.lorentzen@gmail.com>
2022-05-01 16:38:44 -0400

    Create neatworker.py
    worker script for evaluation machines

---
+++ neatworker.py
@@ -0,0 +1,49 @@
+from shellbot import ShellBot
+from time import sleep
+import pymongo
+import json
+from bson.binary import Binary
+import pickle
+from datetime import datetime, timedelta
+from neat import nn
+
+try:
+    with open('creds.json') as f:
+        creds = json.load(f)
+except FileNotFoundError:
+    print('creds.json not found!')
+    exit()
+
+db_string = creds['mongodb']
+client = pymongo.MongoClient(db_string)
+db = client.NEAT
+collection = db.genomes
+
+print('Starting Bot!')
+sb = ShellBot()
+sb.start()
+## Give Us Enough Time to Type Password In
+for idx in range(0, 10):
+    print(f'Grant Operator Perms in Next {10 - idx} Seconds!!!')
+    sleep(1)
+
+while True:
+    if collection.count_documents({'started_eval': False}) != 0:
+        genome = collection.find_one({'started_eval': False})
+        collection.update_one({'_id': genome['_id']}, {'$set': {'started_eval': True, 'started_at': datetime.now()}})
+        net: nn.FeedForwardNetwork = pickle.loads(genome['genome'])
+        generation = genome['generation']
+        individual_num = genome['individual_num']
+        sb.nn == net
+        sb.reset()
+        print(f'Generation {generation} number {individual_num} started evaluation!')
+        sleep(60)
+        fitness = sb.score
+        collection.update_one({'_id': genome['_id']}, {'$set': {'fitness': fitness, 'finished_eval': True}})
+        print(f'Generation {generation} number {individual_num} finished evaluation! Fitness: {fitness}')
+        print("==================")
+    else:
+        print('Waiting For Genomes...')
+        sleep(30)
+    sleep(1)
+
