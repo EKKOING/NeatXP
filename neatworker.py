e80b385a239d249146312ffae18a8840e47a536f
EKKOING <nicholas.lorentzen@gmail.com>
2022-05-04 00:34:48 -0400

    add bonuses

--- neatworker.py
+++ neatworker.py
@@ -29,32 +29,38 @@ sb.start()
 for idx in range(0, 10):
     print(f'Grant Operator Perms in Next {10 - idx} Seconds!!!')
     sleep(1)
-
+print("=== Beginning Work Cycle ===")
 waiting = False
 while True:
-    if collection.count_documents({'started_eval': False}) != 0:
-        genome = collection.find_one({'started_eval': False})
-        collection.update_one({'_id': genome['_id']}, {
-                              '$set': {'started_eval': True, 'started_at': datetime.now()}})
-        net: nn.FeedForwardNetwork = pickle.loads(genome['genome'])
-        generation = genome['generation']
-        individual_num = genome['individual_num']
-        sb.nn = net
-        sb.reset()
-        print(
-            f'Generation {generation} number {individual_num} started evaluation!')
-        sleep(60)
-        fitness = sb.score
-        collection.update_one({'_id': genome['_id']}, {
-                              '$set': {'fitness': fitness, 'finished_eval': True}})
-        print(
-            f'Generation {generation} number {individual_num} finished evaluation! Fitness: {fitness}')
-        print("==================")
-        waiting = False
-    else:
-        if not waiting:
-            print('Waiting For Work Assignment!')
-            waiting = True
-        ## Try to desync workers
+    try:
+        if collection.count_documents({'started_eval': False}) != 0:
+            genome = collection.find_one({'started_eval': False})
+            collection.update_one({'_id': genome['_id']}, {
+                                '$set': {'started_eval': True, 'started_at': datetime.now()}})
+            net: nn.FeedForwardNetwork = pickle.loads(genome['genome'])
+            generation = genome['generation']
+            individual_num = genome['individual_num']
+            sb.nn = net
+            sb.reset()
+            print(
+                f'Generation {generation} number {individual_num} started evaluation!')
+            sleep(eval_length)
+            fitness = sb.score
+            bonus = sb.cum_bonus
+            collection.update_one({'_id': genome['_id']}, {
+                                '$set': {'fitness': fitness, 'bonus': bonus, 'finished_eval': True}})
+            print(
+                f'Generation {generation} number {individual_num} finished evaluation! Fitness: {fitness}')
+            print("==================")
+            waiting = False
+        else:
+            if not waiting:
+                print('Waiting For Work Assignment!')
+                waiting = True
+            ## Try to desync workers
+            sleep(randint(5, 15))
+    except Exception as e:
+        print(f'Error: {e}')
         sleep(randint(5, 15))
+        print('==================')
     sleep(1)
