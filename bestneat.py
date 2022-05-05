34d8fde4951585532f0750b3589aa6f0f040e683
EKKOING <nicholas.lorentzen@gmail.com>
2022-05-05 14:20:58 -0400

    Create bestneat.py

---
+++ bestneat.py
@@ -0,0 +1,45 @@
+from shellbot import ShellBot
+from time import sleep
+import pymongo
+import json
+from bson.binary import Binary
+import pickle
+from neat import nn
+from random import randint
+
+eval_length = 15
+
+try:
+    with open('creds.json') as f:
+        creds = json.load(f)
+except FileNotFoundError:
+    print('creds.json not found!')
+    exit()
+
+db_string = creds['mongodb']
+client = pymongo.MongoClient(db_string)
+db = client.NEAT
+collection = db.genomes
+
+print('Starting Bot!')
+sb = ShellBot()
+sb.start()
+
+while True:
+    try:
+        genome = collection.find({'started_eval': True, 'finished_eval': True}).sort('fitness', pymongo.DESCENDING).limit(15)
+        skip_amt = randint(0, 10)
+        for _idx in range(0, skip_amt):
+            genome.next()
+
+        net: nn.FeedForwardNetwork = pickle.loads(genome['genome'])
+        generation = genome['generation']
+        individual_num = genome['individual_num']
+        sb.nn = net
+        print(
+            f'Generation {generation} number {individual_num} loaded!')
+        sleep(15)
+    except Exception as e:
+        print(f'Error: {e}')
+        print('==================')
+    sleep(1)
