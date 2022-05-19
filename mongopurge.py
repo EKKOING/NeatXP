160556db817627cca5257010fe2e40e231ed0ddc
EKKOING <nicholas.lorentzen@gmail.com>
2022-05-19 03:05:34 -0400

    patch in algo key for neat

--- mongopurge.py
+++ mongopurge.py
@@ -7,7 +7,7 @@ with open('creds.json') as f:
     creds = json.load(f)
 
 from_gen = 0
-to_gen = 200
+to_gen = 400
 preserve_top = 10
 
 client = pymongo.MongoClient(creds['mongodb'])
@@ -16,7 +16,13 @@ db = client.NEAT
 for generation in range(from_gen, to_gen):
     print(f'Purging Generation {generation}')
     for genome in db.genomes.find({'generation': generation}).sort('fitness', pymongo.DESCENDING).skip(preserve_top):
-        print(f'Purging {genome["key"]}')
-        db.genomes.update_one({'_id': genome['_id']}, {'$set': {'genome': None}})
-    sleep(1)
+        if genome['genome']:
+            print(f'Purging {genome["generation"]}-{genome["individual_num"]}')
+            db.genomes.update_one({'_id': genome['_id']}, {'$set': {'genome': None}})
+    ##sleep(1)
+
+for genome in db.genomes.find({'algo': None}):
+    print(f'Fixing {genome["generation"]}-{genome["individual_num"]}')
+    db.genomes.update_one({'_id': genome['_id']}, {'$set': {'algo': 'NEAT'}})
+
 
